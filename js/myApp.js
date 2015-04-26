c0c0054f5b76a845dc4c3eb6e4ba389f3da51362
unknown <dan.dh.lee@gmail.com>
2015-04-26 16:26:09 -0700

    Add gender setting button

--- js/myApp.js
+++ js/myApp.js
@@ -2,4 +2,26 @@ angular.module('myApp', [])
 	.constant('VERSION', 1.0)
 	.controller('MyCtrl', function(VERSION, $scope) {
 		$scope.version = VERSION;
+		var male = {
+			subjectPronoun: 'he',
+			objectPronoun: 'him',
+			possessivePronoun: 'his'
+		};
+		var female = {
+			subjectPronoun: 'she',
+			objectPronoun: 'her',
+			possessivePronoun: 'her'
+		};
+		$scope.gender = female;
+		$scope.change = function() {
+			if ($scope.gender == female) {
+				document.getElementById("name").setAttribute("placeholder",
+				"Male Name");
+				$scope.gender = male;
+			} else {
+				document.getElementById("name").setAttribute("placeholder",
+				"Female Name");
+				$scope.gender = female;
+			}
+		};
 	});
